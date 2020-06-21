933849b41a3f0173cb1005aeff140795d444dea3
Duncan Werner <dwerner@riskamp.com>
2020-06-21 08:05:19 -0700

    add, then remove, favicon as datauri

--- treb-embed/src/composite-sheet.ts
+++ treb-embed/src/composite-sheet.ts
@@ -278,15 +278,23 @@ export class CompositeSheet {
     if (!new_window) { return; }
 
     new_window.document.title = this.sheet.document_name ?
-      `TREB: ${this.sheet.document_name}` : 'TREB';
+      `TREB: ${this.sheet.document_name}` : 'TREB: Untitled Document';
 
     // this is synchronous, right? we determined that when we
     // were testing yield() implementations.
 
+    // <link rel="icon" type="image/svg+xml" href="/leaf.svg">
+
     const link = new_window.document.createElement('link');
-    link.setAttribute('rel', 'shortcut icon');
-    link.setAttribute('type', 'image/x-icon');
-    link.setAttribute('href', 'https://treb.app/favicon.ico')
+    link.setAttribute('rel', 'icon');
+    link.setAttribute('type', 'image/svg+xml');
+
+    // either one of these works in firefox, but neither works in chrome.
+    // haven't checked safari/edge.
+
+    // link.setAttribute('href', `data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1 Basic//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11-basic.dtd'%3E%3Csvg version='1.1' baseProfile='basic' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='64px' height='64px' viewBox='0 0 64 64' xml:space='preserve'%3E%3Cpath fill='%238CC63F' d='M37.913,14.323c-2.042,0-7.067,2.558-8.72,3.481c-0.959-1.012-1.065-2.522-1.243-4.475 c-0.959,0.994-0.337,4.014,0,5.115c-4.19,3.125-7.707,6.357-11.295,10.016c-1.225-1.936-2.06-3.517-2.344-7.033 c-1.243,2.664,0.355,6.163,1.278,8.098c-3.96,4.99-7.885,10.354-11.064,15.486C-10.001,14.323,34.344-3.916,63.327,8.641 c-17.955,11.952-22.59,49.672-54.13,39.639c-2.22,3.197-3.712,7.37-5.541,11.082c-1.527,0.107-2.593-0.675-2.983-1.278 c3.072-7.441,7.033-13.995,11.082-20.459c4.387,0.125,8.737,0.195,12.36-0.426c-3.144-0.834-6.908-0.319-10.655-1.278 c2.291-4.387,5.63-7.726,8.95-11.082c3.605,0.32,7.264,1.314,11.082,0.426c-3.32-0.586-6.535-0.799-9.377-1.705 C27.223,20.131,33.438,16.401,37.913,14.323z'/%3E%3C/svg%3E%0A`);
+    link.setAttribute('href', 'https://treb.app/leaf.svg');
+
     new_window.document.head.appendChild(link);
 
     const treb_script = new_window.document.createElement('script');
@@ -431,6 +439,7 @@ export class CompositeSheet {
 
     script.textContent = `(function(){var options=${JSON.stringify(target_options)};var attempts=0;var document_data=${document_data};var load=function(){options.container=document.querySelector('.treb');if(options.container&&window.TREB){window.sheet=TREB.CreateSpreadsheet(options);sheet.LoadDocument(document_data);}else{if(++attempts<32){setTimeout(function(){load();},100);}}};load();})();`;
     new_window.document.body.appendChild(script);
+    new_window.document.close();
 
   }
 
