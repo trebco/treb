f05ce0f34dd825ebaea9a24925be15cd4b049280
Duncan Werner <dwerner@riskamp.com>
2020-07-06 15:13:35 -0700

    postcss fix

--- postcss.config.js
+++ postcss.config.js
@@ -10,6 +10,9 @@ module.exports = {
     /** we have one file that uses calc with variables, this reduces that to constants */
     'postcss-calc': {},
 
+    /** this is for charts only. TODO: preprocess colors */
+    'postcss-color-mod-function': {},
+
     /** general future proofing */
     'postcss-preset-env': {},
 
