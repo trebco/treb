594ab0bf8662116f4b8274989780332c7008de8c
Duncan Werner <dwerner@riskamp.com>
2020-08-10 14:19:43 -0700

    add some stats functions

--- treb-calculator/src/functions/statistics-functions.ts
+++ treb-calculator/src/functions/statistics-functions.ts
@@ -1,9 +1,119 @@
 
 import { FunctionMap } from '../descriptors';
 import * as Utils from '../utilities';
-import { ValueError } from '../function-error';
+import { ValueError, FunctionError } from '../function-error';
+
+const Variance = (data: number[], sample = false) => {
+
+  const len = data.length;
+  
+  let m = 0;
+  let v = 0;
+  // let k = 0;
+  // let s = 0;
+
+  for (let i = 0; i < len; i++) m += data[i];
+
+  m /= len;
+  // const mean = m;
+
+  for (let i = 0; i < len; i++) {
+    const d = data[i] - m;
+    v += (d * d);
+    // s += (d * d * d);
+    // k += (d * d * d * d);
+  }
+
+  // const N = len;
+  // const variance = v / len;
+  // const stdev = Math.sqrt(v / len);
+  return sample ? (v / (len-1)) : (v/len);
+
+};
 
 export const StatisticsFunctionLibrary: FunctionMap = {
+
+  StDev: {
+    description: 'Returns the standard deviation of a set of values, corresponding to a population',
+    arguments: [{ name: 'data', }],
+    fn: (...args: any[]) => {
+      return Math.sqrt(Variance(Utils.Flatten(args), false));
+    },
+  },
+
+  'StDev.S': {
+    description: 'Returns the standard deviation of a set of values, corresponding to a sample of a population',
+    arguments: [{ name: 'data', }],
+    fn: (...args: any[]) => {
+      return Math.sqrt(Variance(Utils.Flatten(args), true));
+    },
+  },
+
+  Var: {
+    description: 'Returns the variance of a set of values, corresponding to a population',
+    arguments: [{ name: 'data', }],
+    fn: (...args: any[]) => {
+      return Variance(Utils.Flatten(args), false);
+    },
+  },
+
+  'Var.S': {
+    description: 'Returns the variance of a set of values, corresponding to a sample of a population',
+    arguments: [{ name: 'data', }],
+    fn: (...args: any[]) => {
+      return Variance(Utils.Flatten(args), true);
+    },
+  },
+
+  Correl: {
+    description: 'Returns the correlation between two ranges of values',
+    arguments: [{
+      name: 'A',
+    }, {
+      name: 'B',
+    }],
+    fn: (x: any[], y: any[]): number|FunctionError => {
+
+      // both must be 2d arrays, we're assuming the same or mostly similar shape
+      if (!Array.isArray(x) || !Array.isArray(y)) { return ValueError; }
+      if (!Array.isArray(x[0]) || !Array.isArray(y[0])) { return ValueError; }
+      
+      let rslt = 0;
+      let sumProduct = 0;
+      let sumX = 0;
+      let sumY = 0;
+      let sumSquaredX = 0;
+      let sumSquaredY = 0;
+      let count = 0;
+
+      for (let j = 0; j < x.length; j++) {
+        if (!x[j] || !y[j]) { continue; }
+
+        const len = x[j].length;
+        for (let i = 0; i < len; i++) {
+          const a = Number(x[j][i]);
+          const b = Number(y[j][i]);
+
+          if (isNaN(a) || isNaN(b)) { continue; }
+
+          sumProduct += (a * b);
+          sumX += a;
+          sumY += b;
+          sumSquaredX += (a * a);
+          sumSquaredY += (b * b);
+          count++;
+        }
+      }
+  
+      rslt = ((count * sumProduct) - (sumX * sumY));
+      if (rslt) {
+        rslt /= Math.sqrt(((count * sumSquaredX) - (sumX * sumX)) * ((count * sumSquaredY) - (sumY * sumY)));
+      }
+  
+      return rslt;
+
+    },
+  },
 
   GeoMean: {
     fn: (...args: any[]) => {
@@ -79,4 +189,6 @@ export const StatisticsFunctionLibrary: FunctionMap = {
 
 export const StatisticsFunctionAliases: {[index: string]: string} = {
   Mean: 'Average',
+  'StDev.P': 'StDev',
+  'Var.P': 'Var',
 };
