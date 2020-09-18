617b0253ff9225ffad86f480daf1f601d62eb6c5
duncan werner <dwerner@riskamp.com>
2020-09-18 16:21:34 -0700

    add standalone build for charts
    this includes formatter, so you don't need both

---
+++ treb-charts/src/index-standalone.ts
@@ -0,0 +1,8 @@
+
+
+export { Chart } from './chart';
+export { ChartFunctions } from './chart-functions';
+
+export { Localization } from 'treb-base-types';
+export { NumberFormatCache } from 'treb-format';
+
