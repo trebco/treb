9e41fef97d5679249ec2ce952a5e4f1b86ecb2c9
duncan werner <dwerner@riskamp.com>
2021-02-26 09:14:01 -0800

    clean up date parsing
    date parsing was too aggressive, and if you entered a string like "Task 1" it
    would be parsed as a date. we add some rules to try to clean this up, but it's
    still a WIP. TODO: check L10N.

--- treb-format/src/value_parser.ts
+++ treb-format/src/value_parser.ts
@@ -44,6 +44,144 @@ const this_year = new Date().getUTCFullYear();
  */
 class ValueParserType {
 
+  public compare_day?: Record<string, number>;
+  public compare_month?: Record<string, number>;
+
+  public TestDate(text: string): number|false {
+
+    // first check if JS can turn it into a date at all. we can
+    // use that as a baseline/initial check (in any event there's
+    // no point in doing the work otherwise)
+
+    const date_value = Date.parse(text);
+    if (isNaN(date_value)) {
+      // console.info('td false: is nan')
+      return false;
+    }
+
+    const date = new Date(date_value);
+
+    // OK so there's a date, now we can do whatever validation we want
+
+    // is there a regexp for accented characters? actually we can
+    // maybe go backwards... remove things that properly belong in
+    // dates: numbers, dashes and slashes (+ comma, whitespace)
+
+    // Q: what does lowercase do for accented characters? (...)
+
+    const tmp = text.replace(/[\d\-\\/,\s]+/g, ' ').toLocaleLowerCase();
+
+    // then split into individual strings. trim and drop empty
+
+    const components = tmp.split(/\s+/).map(component => component.trim()).filter(component => !!component);
+
+    if (!components.length) {
+      // console.info('td true: no strings');
+      return date_value; // probably a date
+    }
+
+    // now we'll compare these to stuff we have in l10n. rule (WIP):
+    // should be in month or day-of-week. accept short or long, or one 
+    // character. (one character introduces some ambiguity...)
+
+    // drop 1 character, it's ambiguous and annoying (also hard to handle)
+
+    // so basically, any string in here has to be a month or day; we can't 
+    // have two of either; they have to match what was parsed; and you can't
+    // have a day-of-week but not a month.
+
+    // FIXME: cache/precalc
+    // let's do it lazily
+
+    if (!this.compare_month) {
+      this.compare_month = {};
+      for (let i = 0; i < 12; i++) {
+        this.compare_month[Localization.date_components.long_months[i].toLocaleLowerCase()] = i;
+        this.compare_month[Localization.date_components.short_months[i].toLocaleLowerCase()] = i;
+        // comparison[Localization.date_components.long_months[i][0].toLocaleLowerCase()] = i;
+      }
+    }
+
+    if (!this.compare_day) {
+      this.compare_day = {};
+      for (let i = 0; i < 7; i++) {
+        this.compare_day[Localization.date_components.long_days[i].toLocaleLowerCase()] = i;
+        this.compare_day[Localization.date_components.short_days[i].toLocaleLowerCase()] = i;
+      }
+    }
+
+    let found_month = false;
+    let found_day = false;
+
+    for (const component of components) {
+      let found = false;
+      for (const [month, value] of Object.entries(this.compare_month)) {
+        if (component === month) {
+        
+          // can't have two months in a single date
+          if (found_month) {
+            // console.info('td false: two months')
+            return false;
+          }
+
+          // have a string match [FIXME: fuzzy?]
+          // check that the month matches
+
+          if (date.getUTCMonth() !== value) {
+            // console.info('td false: month mismatch')
+            return false;
+          }
+
+          found = true;
+          found_month = true;
+
+        }
+      }
+      if (!found) {
+        for (const [day, value] of Object.entries(this.compare_day)) {
+          if (component === day) {
+
+            // can't have two days either
+            if (found_day) {
+              // console.info('td false: two days')
+              return false;
+            }
+
+            if (date.getUTCDay() !== value) {
+              // console.info('td false: day mismatch')
+              return false;
+            }
+  
+            found = true;
+            found_day = true;
+          }
+        }
+      }
+
+      if (!found) {
+
+        // whatever this string is, we don't recognize it. so this is 
+        // probably not a date.
+
+        // console.info('td false: unmatched string')
+        return false;
+      }
+    }
+    
+    // last check: no DOW without month.
+
+    if (found_day && !found_month) {
+      // console.info('td false: day but no month')
+      return false;
+    }
+
+    // OK, accept it
+
+    // console.info('td true: ran out of cases')
+    return date_value;
+
+  }
+
   /**
    * parse a string. if it can reasonably be converted to a number,
    * do that and return the number; otherwise return the original
@@ -112,9 +250,17 @@ class ValueParserType {
       // check date, but bound on reasonable years...
       // also maybe parameterize, make this optional
 
-      const date = Date.parse(text);
+      // this is too aggressive (at least in chrome). we need to dial it 
+      // back a bit. to do that, we will validate any strings in the text
+      // and ensure they look like date components (usually months and days),
+      // within the current locale.    
 
-      if (!isNaN(date)) {
+      const date = this.TestDate(text);
+
+      if (false !== date && !isNaN(date)) {
+
+        // we can drop this bit, now (I think)
+
         const check = new Date(date);
         const year = check.getUTCFullYear();
 
