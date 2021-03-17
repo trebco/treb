edf05ce6159e232324d2a9d7bd7272ac92048ab4
duncan werner <dwerner@riskamp.com>
2021-03-17 08:14:23 -0700

    fix sparkline layout

--- treb-calculator/src/functions/sparkline.ts
+++ treb-calculator/src/functions/sparkline.ts
@@ -231,7 +231,7 @@ export class Sparkline {
 
     if (values.length) {
 
-      const step = (width - 2 * x_margin) / values.length;
+      const step = (width - 2 * x_margin - 2) / (values.length-1);
       const pixel_range = (height - 2 * y_margin); // ?
       const base = y_margin;
 
