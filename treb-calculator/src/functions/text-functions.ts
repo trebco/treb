2cdd9a7613397e11aa3dddcbfedd26051def0dc9
duncan werner <dwerner@riskamp.com>
2021-04-27 17:35:59 -0700

    add string search functions
    FIND and SEARCH. the latter is icase and has wildcards.

--- treb-calculator/src/functions/text-functions.ts
+++ treb-calculator/src/functions/text-functions.ts
@@ -3,6 +3,62 @@ import { FunctionMap } from '../descriptors';
 import { NumberFormatCache } from 'treb-format';
 import { Localization, UnionValue, ValueType } from 'treb-base-types';
 import * as Utils from '../utilities';
+import { ValueError } from '../function-error';
+
+/**
+ * parse a string with wildcards into a regex pattern
+ * 
+ * from
+ * https://exceljet.net/glossary/wildcard
+ * 
+ * Excel has 3 wildcards you can use in your formulas:
+ *
+ * Asterisk (*) - zero or more characters
+ * Question mark (?) - any one character
+ * Tilde (~) - escape for literal character (~*) a literal question mark (~?), or a literal tilde (~~)
+ * 
+ * they're pretty liberal with escaping, nothing is an error, just roll with it
+ * 
+ */
+const ParseWildcards = (text: string): string => {
+
+  const result: string[] = [];
+  const length = text.length;
+
+  const escaped_chars = '[\\^$.|?*+()';
+
+  for (let i = 0; i < length; i++) {
+    let char = text[i];
+    switch (char) {
+
+      case '*':
+        result.push('.', '*');
+        break;
+
+      case '?':
+        result.push('.');
+        break;
+
+      case '~':
+        char = text[++i] || '';
+      
+      // eslint-disable-next-line no-fallthrough
+      default:
+        for (let j = 0; j < escaped_chars.length; j++) {
+          if (char === escaped_chars[j]) {
+            result.push('\\');
+            break;
+          }
+        }
+        result.push(char);
+        break;
+
+    }
+  }
+
+  return result.join('');
+
+};
 
 export const TextFunctionLibrary: FunctionMap = {
 
@@ -94,7 +150,87 @@ export const TextFunctionLibrary: FunctionMap = {
 
   */
 
- Concatenate: {
+  /**
+   * shame we can't write a proper search function, but we need to be 
+   * consistent. some notes:
+   * 
+   * FIND is case-sensitive and does not support wildcards
+   * SEARCH is icase and supports wildcards
+   * 
+   * re: wildcards, from
+   * https://exceljet.net/glossary/wildcard
+   * 
+   * Excel has 3 wildcards you can use in your formulas:
+   *
+   * Asterisk (*) - zero or more characters
+   * Question mark (?) - any one character
+   * Tilde (~) - escape for literal character (~*) a literal question mark (~?), or a literal tilde (~~)
+   * 
+   * start index is 1-based, and defaults to 1; < 1 is an error. if the string
+   * is not found, that's an error. if needle is empty, return start.
+   * 
+   */
+  Search: {
+    description: 'Find a string (needle) in another string (haystack). Case-insensitive.',
+    arguments: [
+      { name: 'Needle', },
+      { name: 'Haystack', },
+      { name: 'Start', default: 1, },
+    ],
+    fn: (needle: string, haystack: string, start = 1): UnionValue => {
+      if (start >= 1) {
+        if (!needle) {
+          return {
+            type: ValueType.number, value: start,
+          }
+        }
+
+        // translate into regex. do we need an actual parser for this, or 
+        // can we get by with regexes? should we have some sort of cache
+        // for common patterns?
+
+        const pattern = ParseWildcards(needle);
+        // console.info('n', needle, 'p', pattern);
+        const match = new RegExp(pattern, 'i').exec(haystack.substr(start - 1));
+
+        if (match) {
+          return {
+            type: ValueType.number, value: match.index + start,
+          }
+        }
+
+      }
+      return ValueError();
+    },
+  },
+
+  Find: {
+    description: 'Find a string (needle) in another string (haystack). Case-sensitive.',
+    arguments: [
+      { name: 'Needle', },
+      { name: 'Haystack', },
+      { name: 'Start', default: 1, },
+    ],
+    fn: (needle: string, haystack: string, start = 1): UnionValue => {
+      if (start >= 1) {
+        if (!needle) {
+          return {
+            type: ValueType.number, value: start,
+          }
+        }
+        const match = new RegExp(needle).exec(haystack.substr(start - 1));
+        if (match) {
+          return {
+            type: ValueType.number, value: match.index + start,
+          }
+        }
+      }
+      return ValueError();
+    },
+  },
+
+  /** canonical should be CONCAT; concatenate can be an alias */
+ Concat: {
   description: 'Pastes strings together',
   fn: (...args: unknown[]): UnionValue => {
 
@@ -122,6 +258,6 @@ export const TextFunctionLibrary: FunctionMap = {
 };
 
 export const TextFunctionAliases: {[index: string]: string} = {
-  Concat: 'Concatenate',
+  Concatenate: 'Concat',
 };
 
