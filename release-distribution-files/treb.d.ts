0a33fb04543dfb61b879464ff4f0fd3fd4421ddc
duncan <dwerner@riskamp.com>
2022-05-15 07:43:35 -0700

    update version number

--- release-distribution-files/treb.d.ts
+++ release-distribution-files/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v18.8. Copyright 2018-2022 Structured Data, LLC. All rights reserved. CC BY-ND: https://treb.app/license */
+/*! API v18.9. Copyright 2018-2022 Structured Data, LLC. All rights reserved. CC BY-ND: https://treb.app/license */
 
 /** 
  * Global instance. In the base script, this object will be created as an
