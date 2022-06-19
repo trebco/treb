46b4ad63d38c6408ff51c1d5ecafc33be263f920
duncan <dwerner@riskamp.com>
2022-06-19 06:55:40 -0700

    dynamic port

--- treb-ws/server/src/index.ts
+++ treb-ws/server/src/index.ts
@@ -1,6 +1,14 @@
 
 import { WSServer } from './server';
 
-new WSServer(9001);
+let port = 9001;
 
+for (let i = 0; i < process.argv.length; i++) {
+  if (process.argv[i] === '--port') {
+    port = Number(process.argv[++i]);
+  }
+}
+
+console.info(`starting server on port ${port}`);
+new WSServer(port);
 
