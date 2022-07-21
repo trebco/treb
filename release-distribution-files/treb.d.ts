4d26bd9292159222d0ca2f4b98287b1e632b7287
duncan <dwerner@riskamp.com>
2022-07-20 18:06:20 -0700

    fix banners to clean up API
    we were accidentally pulling in license banners in the API. switching
    style seems to have fixed that.

--- release-distribution-files/treb.d.ts
+++ release-distribution-files/treb.d.ts
@@ -673,73 +673,7 @@ export interface FreezePane {
     rows: number;
     columns: number;
 }
-
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
- 
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
 export declare type BorderConstants = "none" | "all" | "outside" | "top" | "bottom" | "left" | "right" | "double-top" | "double-bottom";
-
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
 
 /**
  * options for serializing data
@@ -762,28 +696,6 @@ export interface SerializeOptions {
     /** prune unused rows/columns */
     shrink?: boolean;
 }
-
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
 
 /**
  * Structure represents a cell address. Note that row and column are 0-based.
@@ -813,28 +725,6 @@ export interface Point {
     y: number;
 }
 
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
-
 /** structure represents rectangle coordinates */
 export interface IRectangle {
     top: number;
@@ -842,28 +732,6 @@ export interface IRectangle {
     width: number;
     height: number;
 }
-
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
 export declare namespace Style {
 
     /**
@@ -972,28 +840,6 @@ export declare namespace Style {
     }
 }
 export declare type CellValue = undefined | string | number | boolean | Complex | DimensionedQuantity;
-
-/**
- *
- * This file is part of TREB.
- *
- * TREB is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * TREB is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Foobar. If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright 2022 trebco, llc.
- * info@treb.app
- *
- */
 
 /**
  * Complex number type
--- treb-base-types/src/rectangle.ts
+++ treb-base-types/src/rectangle.ts
@@ -1,5 +1,4 @@
-/**
- * 
+/*
  * This file is part of TREB.
  *
  * TREB is free software: you can redistribute it and/or modify it under the 
