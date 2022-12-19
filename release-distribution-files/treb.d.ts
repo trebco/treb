42fcab4a4a2e511d68e3955d347937a064da828f
duncan <dwerner@riskamp.com>
2022-12-18 16:12:37 -0800

    update support files

--- release-distribution-files/treb.d.ts
+++ release-distribution-files/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v22.14. Copyright 2018-2022 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
+/*! API v22.15. Copyright 2018-2022 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
 
 /** 
  * Global instance. In the base script, this object will be created as an
@@ -1055,6 +1055,24 @@ export interface EmbeddedSpreadsheetOptions {
 
     /** target window for hyperlinks (default _blank); set false to disable hyperlinks altogether */
     hyperlinks?: string | false;
+
+    /**
+     * enable handling complex numbers in function calculation. turning this
+     * off doesn't actually disable complex numbers. it means that functions
+     * will not return complex numbers unless one of the arguments is complex.
+     *
+     * for example, if complex numbers are off, `=SQRT(-1)` will return `#VALUE`.
+     * if complex numbers are on, `=SQRT(-1)` will return `i`.
+     *
+     * even if complex numbers are off, however, `=SQRT(-1 + 0i)` will return
+     * `i` because the argument is complex.
+     *
+     * currently this behavior applies to `SQRT`, `POWER` and the exponentiation
+     * operator `^`.
+     *
+     * in version 22, this defaults to `off`.
+     */
+    complex?: 'on' | 'off';
 
     /**
      * for rendering the imaginary number. this is intended to support
