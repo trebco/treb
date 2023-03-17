4f81220b93de64c02186519de66965327463f1b2
duncan <dwerner@riskamp.com>
2023-03-17 14:02:34 -0700

    update build artefacts

--- build/treb.d.ts
+++ build/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v25.0.0-rc3. Copyright 2018-2023 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
+/*! API v25.0. Copyright 2018-2023 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
 
 /**
  * add our tag to the map
