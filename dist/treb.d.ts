7f84f5432977b22d203f69d6c6dca24cafd9f8a3
duncan <dwerner@riskamp.com>
2023-10-13 06:16:07 -0700

    add "light" build to the release module
    the light version removes XLSX import/export. that saves a lot of bytes.

--- dist/treb.d.ts
+++ dist/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v27.12. Copyright 2018-2023 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
+/*! API v28.0. Copyright 2018-2023 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
 
 /**
  * add our tag to the map
