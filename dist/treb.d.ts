888a94cd0341f305c3bbacea4b3b31ed3e6e88d8
duncan <dwerner@riskamp.com>
2024-09-27 15:18:24 -0700

    update build

--- dist/treb.d.ts
+++ dist/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v31.2. Copyright 2018-2024 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
+/*! API v31.3. Copyright 2018-2024 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
 
 /**
  * add our tag to the map
