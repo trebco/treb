94e30bca89c1b850d8b6bf34b3dc135b6bc8b4e9
duncan <dwerner@riskamp.com>
2024-11-18 08:15:28 -0800

    rebuild for sass updates

--- dist/treb.d.ts
+++ dist/treb.d.ts
@@ -1,4 +1,4 @@
-/*! API v31.8. Copyright 2018-2024 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
+/*! API v31.9. Copyright 2018-2024 trebco, llc. All rights reserved. LGPL: https://treb.app/license */
 
 /**
  * add our tag to the map
