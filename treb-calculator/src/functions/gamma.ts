1db8e8a2a373e88ca0e34a1a1a4fc42c013a276d
duncan <dwerner@riskamp.com>
2024-12-25 09:32:00 -0800

    add banner

--- treb-calculator/src/functions/gamma.ts
+++ treb-calculator/src/functions/gamma.ts
@@ -1,8 +1,29 @@
-/**
- * functions and constants for gamma distribution. we're
- * returning false instead of throwing so we can return
- * a spreadsheet-style eror. TODO: optional?
+/*
+ * This file is part of TREB.
+ *
+ * TREB is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any 
+ * later version.
+ *
+ * TREB is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with TREB. If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * Copyright 2022-2024 trebco, llc. 
+ * info@treb.app
+ * 
  */
+
+//
+// functions and constants for gamma distribution. we're
+// returning false instead of throwing so we can return
+// a spreadsheet-style eror. TODO: optional?
+//
 
 const max_iterations = 1000;
 const epsilon = 3.0e-9;
