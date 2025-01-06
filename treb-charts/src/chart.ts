e34a95550e5004f5ec69795e41e6d4874a2aa3b4
duncan <dwerner@riskamp.com>
2025-01-06 11:50:27 -0800

    update banners

--- treb-charts/src/chart.ts
+++ treb-charts/src/chart.ts
@@ -1,3 +1,24 @@
+/*
+ * This file is part of TREB.
+ *
+ * TREB is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any 
+ * later version.
+ *
+ * TREB is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with TREB. If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * Copyright 2022-2025 trebco, llc. 
+ * info@treb.app
+ * 
+ */
+
 
 import type { ChartRenderer } from './renderer-type';
 import type { ChartData } from './chart-types';
