2318bc793d07abc914d8213e7d6d44af9b7609b2
duncan <dwerner@riskamp.com>
2026-01-12 09:37:51 -0800

    update banners

--- treb-calculator/src/functions/normal.ts
+++ treb-calculator/src/functions/normal.ts
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License along 
  * with TREB. If not, see <https://www.gnu.org/licenses/>. 
  *
- * Copyright 2022-2025 trebco, llc. 
+ * Copyright 2022-2026 trebco, llc. 
  * info@treb.app
  * 
  */
